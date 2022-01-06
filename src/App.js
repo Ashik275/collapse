8b9bb5ca2e592e47b504aedad8f1cefe094f731c
Ashik275 <karzon275@gmail.com>
2022-01-06 20:08:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+  const commandRef = useRef();
+
+
+
+  const handleInput = e => {
+    const command = commandRef.current.value;
+    const newCommand = { command: command }
+
+    e.preventDefault();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+          Click Here
+        </button>
+      </p>
+      <div className="c">
+        <div className="collapse collapse-horizontal" id="collapseWidthExample">
+          <div className=" card card-body d  box">
+            <form onSubmit={handleInput}>
+              <input ref={commandRef} type="text" />
+              <input type="submit" value="Submit" />
+            </form>
+            <div className="d-flex px-3 py-2 mt-3 border b border-primary rounded">
+              <p className="pe-5  text">https</p>
+              <div className="vertical pe-5"></div>
+              <p className=" text">Command -Go To Home</p>
+            </div>
+            <div className="d-flex px-3 py-2 mt-3 border b border-primary rounded">
+              <p className="pe-5  text">https</p>
+              <div className="vertical pe-5 "></div>
+              <p className=" text">Command -Go To Profile</p>
+            </div>
+            <div className="d-flex px-3 py-2 mt-3 border b border-primary rounded">
+              <p className="pe-5  text">https</p>
+              <div className="vertical pe-5"></div>
+              <p className="text">Command -Go To Cart</p>
+            </div>
+            <div className="d-flex px-3 py-2 mt-3 border b border-primary rounded">
+              <p className="pe-5  text">https</p>
+              <div className="vertical pe-5"></div>
+              <p className="text">Command -Add[FULL NAME PRODUCT TO CART] To Cart</p>
+            </div>
+            <div className="d-flex px-3 py-2 mt-3 border b border-primary rounded">
+              <p className="pe-5  text">https</p>
+              <div className="vertical pe-5"></div>
+              <p className="text"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
